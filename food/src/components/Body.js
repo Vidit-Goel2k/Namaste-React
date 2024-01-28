7851b2bdda5ff151bf43836e9f5bc7a7e4924dd0
Vidit <viditgoel2k@gmail.com>
2024-01-28 18:32:05 +0530

    06.04: used live swiggy api to fetch data

--- food/src/components/Body.js
+++ food/src/components/Body.js
@@ -1,9 +1,25 @@
-import {useState} from 'react'
-import resList from './../utils/mockData';
+import {useState, useEffect} from 'react'
 import RestaurantCard from './RestaurantCard';
+import { SWIGGY_API } from '../utils/constants';
 
 const Body = () => {
-  const [restaurantList, setRestaurantList] = useState(resList)
+  const [restaurantList, setRestaurantList] = useState([])
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
+  
+  const fetchData = async () => {
+    const data = await fetch(SWIGGY_API)
+    const json = await data.json()
+    if(json && json.data.cards[4].card.card.gridElements.infoWithStyle){
+      populateData(json.data.cards[4].card.card.gridElements.infoWithStyle)
+    }
+  }
+
+  const populateData = (swiggyApiData) => {
+    setRestaurantList(swiggyApiData.restaurants)
+  }
 
   const ratingFilter = () => {
     const filteredRestaurantList = restaurantList.filter((restaurant)=> restaurant.data.avgRating >= 4)
@@ -17,7 +33,7 @@ const Body = () => {
       <div className="res-container">
         {
           restaurantList.map((restaurant) => (
-            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           ))
         }
       </div>
--- food/src/utils/constants.js
+++ food/src/utils/constants.js
@@ -1,2 +1,4 @@
 export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
 export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6302312&lng=77.4349177&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const SWIGGY_API_DATA = "data.cards[4].card.card.gridElements.infoWithStyle.restaurants[].info"
