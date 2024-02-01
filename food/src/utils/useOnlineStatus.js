9bb860b80ed70fafbfff597d2fa2224d006cc694
Vidit <viditgoel2k@gmail.com>
2024-02-02 01:48:36 +0530

    09.04: understood about naming convention of custom hooks

--- food/src/utils/useOnlineStatus.js
+++ food/src/utils/useOnlineStatus.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 
+// understood that its not mandatory to name custom hooks starting with 'use' but it is the recommended naming convention
 const useOnlineStatus = () => {
     const [isOnline, setIsOnline] = useState(true)
 
