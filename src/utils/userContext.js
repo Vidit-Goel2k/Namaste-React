df32f616b344cfbc8c6f90aad1296099c37fbee9
Vidit <viditgoel2k@gmail.com>
2024-03-01 01:38:13 +0530

    12.02 - 12.03: understood theoretically and diagramatically the inner working of redux and related jargons

--- src/utils/userContext.js
+++ src/utils/userContext.js
@@ -1,9 +1,12 @@
 import { createContext } from "react";
 
-// understood the need for redux
-
 const userContext = createContext({
 	userName: "Default User",
 });
 
 export default userContext;
+
+// understood the need for redux
+
+// * REDUX
+// * (WRITE DATA) => user clicks on add to cart button and the button's event handler DISPATCH an ACTION to the redux STORE where the REDUCER function is called which modifies / updates the cart SLICE of the redux store | => | (READ DATA) => the cart gets updated in the UI and VIEW LAYER by SUBSCRIBING TO THE STORE through SELECTOR which is responsible to update the component which are subscribed to a store whenever the data in the store changes
