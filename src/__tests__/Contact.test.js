8a44b50135751901553215cccd4fa1dfa1b6253b
Vidit <viditgoel2k@gmail.com>
2024-03-02 02:58:41 +0530

    13.04: learned about describe and it in testing

--- src/__tests__/Contact.test.js
+++ src/__tests__/Contact.test.js
@@ -2,58 +2,60 @@ import { render, screen } from "@testing-library/react";
 import ContactUs from "../components/ContactUs";
 import "@testing-library/jest-dom";
 
-// test("should load contact us component", () => {
-    // Render
+// test/it("should load contact us component", () => {
+// Render
 // 	render(<ContactUs />);
-    // Query
+// Query
 // 	const heading = screen.getByRole("heading");
-    // Assert
+// Assert
 // 	expect(heading).toBeInTheDocument();
 // });
 
-test("should load contact us component", () => {
-	render(<ContactUs />);
+describe("Contact Us Page Test Cases", () => {
+	test("should load contact us component", () => {
+		render(<ContactUs />);
 
-	const heading = screen.getByRole("heading");
+		const heading = screen.getByRole("heading");
 
-	expect(heading).toBeInTheDocument();
+		expect(heading).toBeInTheDocument();
+	});
+
+	it("should load button in Contact component", () => {
+		render(<ContactUs />);
+
+		const button = screen.getByRole("button");
+
+		expect(button).toBeInTheDocument();
+	});
+
+	// test("should load random text in Contact component", () => {
+	// 	render(<ContactUs />);
+
+	// 	const randomText = screen.getByText("Random")
+
+	// 	expect(randomText).toBeInTheDocument();
+	// });
+
+	test("should load input name in Contact component", () => {
+		// Render
+		render(<ContactUs />);
+
+		// Querying
+		const inputName = screen.getByPlaceholderText("name");
+
+		// Assert
+		expect(inputName).toBeInTheDocument();
+	});
+
+	test("should load 2 input boxes on the contact component", () => {
+		render(<ContactUs />);
+
+		// Querying
+		const inputBoxes = screen.getAllByRole("textbox");
+
+		// console.log(inputBoxes)
+
+		// Assertion
+		expect(inputBoxes.length).not.toBe(3);
+	});
 });
-
-test("should load button in Contact component", () => {
-	render(<ContactUs />);
-
-	const button = screen.getByRole("button")
-
-	expect(button).toBeInTheDocument();
-});
-
-// test("should load random text in Contact component", () => {
-// 	render(<ContactUs />);
-
-// 	const randomText = screen.getByText("Random")
-
-// 	expect(randomText).toBeInTheDocument();
-// });
-
-test("should load input name in Contact component", () => {
-    // Render
-	render(<ContactUs />);
-
-    // Querying
-	const inputName = screen.getByPlaceholderText("name")
-
-    // Assert
-	expect(inputName).toBeInTheDocument();
-});
-
-test('should load 2 input boxes on the contact component', () => { 
-    render(<ContactUs />)
-
-    // Querying
-    const inputBoxes = screen.getAllByRole("textbox")
-
-    // console.log(inputBoxes)
-
-    // Assertion
-    expect(inputBoxes.length).not.toBe(3)
- })
